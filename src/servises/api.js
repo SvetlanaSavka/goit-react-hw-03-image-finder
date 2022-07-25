aa2216bc66ddf634eae7e9c7ee701e84b8f74b2e
SvetlanaSavka <93277750+SvetlanaSavka@users.noreply.github.com>
2022-07-25 11:16:10 +0300

    recheck

--- src/servises/api.js
+++ src/servises/api.js
@@ -5,23 +5,16 @@ axios.defaults.baseURL = BASE_URL;
 
 export const searchPictures = async (search, page) => {
   try {
-    const respons = await axios.get(
-      '',
-      {
-        params: {
-          q: search,
-          page,
-          key: API_KEY,
-          image_type: 'photo',
-          orientation: 'horizontal',
-          per_page: 12,
-        },
-      }
-
-      // const res = await axios.get('https://httpbin.org/get', { params: { answer: 42 } });
-
-      //axios.get('', { params: { q: search, page, key: API_KEY, image_type: photo, orientation: horizontal, per_page: 12  } });
-    );
+    const respons = await axios.get('', {
+      params: {
+        q: search,
+        page,
+        key: API_KEY,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        per_page: 12,
+      },
+    });
 
     return respons.data;
   } catch (error) {
